13034b990039893ce226c443f92e23c015a7c2a3
Robert Lopez <robert.lopez@yale.edu>
2020-05-15 02:13:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
+  require 'pry'
   # write your code here!
+  new_hash = {} 
+  names = []
+  data.each do |key, value|
+    value.each do |key, value2|
+      names << value2 
 end
+end 
+names.flatten
+binding.pry 
+names.uniq
+names.each do |name|
+  new_hash[name => {}]
+end 
+data.each do |key, value|
+  new_hash[name][key => [] ]
+end 
+
+end 
